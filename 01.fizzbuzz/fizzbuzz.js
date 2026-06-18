cf21fb1fbcc8e2c9631f8deecf5910d586ab4274
oyamairishitomi <h.tezuka0707@gmail.com>
2026-06-18 09:45:14 +0900

    等価演算子を厳密等価演算子に変更

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,9 +1,9 @@
 for (let i = 1; i <= 20; i++) {
-  if (i % 3 == 0 && i % 5 == 0) {
+  if (i % 3 === 0 && i % 5 === 0) {
     console.log("FizzBuzz");
-  } else if (i % 3 == 0) {
+  } else if (i % 3 === 0) {
     console.log("Fizz");
-  } else if (i % 5 == 0) {
+  } else if (i % 5 === 0) {
     console.log("Buzz");
   } else {
     console.log(String(i));
