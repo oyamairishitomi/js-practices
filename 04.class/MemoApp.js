c396a21784e653b8e98abc6adf517f4972a165c7
oyamairishitomi <h.tezuka0707@gmail.com>
2026-06-26 19:56:10 +0900

    argvをoptionsにリネームし、run()から呼ばれないメソッドをprivate化・整理

--- 04.class/MemoApp.js
+++ 04.class/MemoApp.js
@@ -4,29 +4,71 @@ import { MemoStorage } from "./MemoStorage.js";
 
 export class MemoApp {
   constructor() {
-    this.argv = minimist(process.argv.slice(2));
+    this.options = minimist(process.argv.slice(2));
     this.storage = new MemoStorage();
   }
 
   async run() {
-    if (this.argv.l) {
-      this.list();
-    } else if (this.argv.r) {
-      await this.read();
-    } else if (this.argv.d) {
-      await this.delete();
+    if (this.options.l) {
+      this.#list();
+    } else if (this.options.r) {
+      await this.#read();
+    } else if (this.options.d) {
+      await this.#delete();
     } else {
-      await this.add();
+      await this.#add();
     }
   }
 
-  async add() {
-    const content = await this.readStdin();
-    await this.storage.create(content);
+  #list() {
+    const memos = this.storage.findAll();
+    memos.forEach((memo) => {
+      console.log(memo.title);
+    });
   }
 
-  readStdin() {
-    return new Promise((resolve) => {
+  async #read() {
+    const memos = this.storage.findAll();
+    if (memos.length === 0) {
+      console.log("メモがありません");
+      return;
+    }
+    const answer = await inquirer.prompt([
+      {
+        type: "select",
+        name: "memo",
+        message: "Choose a memo you want to see:",
+        choices: memos.map((memo) => ({ name: memo.title, value: memo })),
+      },
+    ]);
+    console.log(answer.memo.content);
+  }
+
+  async #delete() {
+    const memos = this.storage.findAll();
+    if (memos.length === 0) {
+      console.log("メモがありません");
+      return;
+    }
+    const answer = await inquirer.prompt([
+      {
+        type: "select",
+        name: "memo",
+        message: "Choose a memo you want to delete:",
+        choices: memos.map((memo) => ({ name: memo.title, value: memo })),
+      },
+    ]);
+    this.storage.delete(answer.memo.id);
+  }
+
+  async #add() {
+    const content = await this.#readStdin();
+    const memo = await this.storage.create(content);
+    console.log(`${memo.title} を保存しました`);
+  }
+
+  #readStdin() {
+    return new Promise((resolve, ßreject) => {
       const chunks = [];
       process.stdin.setEncoding("utf8");
       process.stdin.on("data", (chunk) => {
@@ -35,47 +77,9 @@ export class MemoApp {
       process.stdin.on("end", () => {
         resolve(chunks.join(""));
       });
+      process.stdin.on("error", (err) => {
+        reject(err);
+      });
     });
-  }
-
-  list() {
-    const memos = this.storage.findAll();
-    memos.forEach((memo) => {
-      console.log(memo.title);
-    });
-  }
-
-  async read() {
-    const memos = this.storage.findAll();
-    if (memos.length === 0) {
-      console.log("メモがありません");
-    } else {
-      const answer = await inquirer.prompt([
-        {
-          type: "select",
-          name: "memo",
-          message: "Choose a note you want to see:",
-          choices: memos.map((memo) => ({ name: memo.title, value: memo })),
-        },
-      ]);
-      console.log(answer.memo.content);
-    }
-  }
-
-  async delete() {
-    const memos = this.storage.findAll();
-    if (memos.length === 0) {
-      console.log("メモがありません");
-    } else {
-      const answer = await inquirer.prompt([
-        {
-          type: "select",
-          name: "memo",
-          message: "Choose a note you want to delete:",
-          choices: memos.map((memo) => ({ name: memo.title, value: memo })),
-        },
-      ]);
-      this.storage.destroy(answer.memo.id);
-    }
   }
 }
