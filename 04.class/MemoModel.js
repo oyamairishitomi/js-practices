1297b2290effc980cab15029669cd4b343d4f9bc
oyamairishitomi <h.tezuka0707@gmail.com>
2026-06-26 20:00:38 +0900

    MemoModelのプロパティをprivate化し、getter経由でのみ読み取れるように修正

--- 04.class/MemoModel.js
+++ 04.class/MemoModel.js
@@ -1,7 +1,21 @@
 export class Memo {
-  constructor(id, title, content) {
-    this.id = id;
-    this.title = title;
-    this.content = content;
+  #id;
+  #content;
+
+  constructor(id, content) {
+    this.#id = id;
+    this.#content = content;
+  }
+
+  get id() {
+    return this.#id;
+  }
+
+  get content() {
+    return this.#content;
+  }
+
+  get title() {
+    return this.#content.split("\n")[0];
   }
 }
